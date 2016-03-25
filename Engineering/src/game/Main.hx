403b52c7e0cbfadae97b3642b3476372e60a8ffe
KnowThyParadox <Lekgold@gmail.com>
2016-03-25 16:21:49 -0400

    Adding a new empty haxe project to get started from.

---
+++ Engineering/src/game/Main.hx
@@ -0,0 +1,21 @@
+package game;
+
+import openfl.display.Sprite;
+import openfl.Lib;
+
+/**
+ * ...
+ * @author Josh Elliott
+ */
+class Main extends Sprite 
+{
+
+	public function new() 
+	{
+		super();
+		
+		// Assets:
+		// openfl.Assets.getBitmapData("img/assetname.jpg");
+	}
+
+}
